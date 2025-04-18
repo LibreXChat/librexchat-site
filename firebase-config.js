47d91a4a2ebfbdf7dd4a70cb0f2d7b879709e0fa
LibreXChat <librexchat@gmail.com>
2025-04-18 23:35:14 +0300

    Update and rename firebase_config.php to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhmuX9aWec4NjxiSZ1FmqBr7LvioyK1rk",
+  authDomain: "librexchat-dba06.firebaseapp.com",
+  projectId: "librexchat-dba06",
+  storageBucket: "librexchat-dba06.appspot.com",
+  messagingSenderId: "48021503169",
+  appId: "1:48021503169:web:fb694c3b1e3416461ae840",
+  measurementId: "G-9K5QGCT108"
+};
+
+// Инициализация Firebase (если используется в этом же файле)
+firebase.initializeApp(firebaseConfig);
+
--- firebase_config.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Firebase эмуляция
-define('FIREBASE_API_KEY', 'AIzaSyBhmuX9aWec4NjxiSZ1FmqBr7LvioyK1rk');
-define('FIREBASE_PROJECT_ID', 'librexchat-dba06');
-?>
